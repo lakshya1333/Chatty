0a9637fe80119da5b61ecc3231db005d242ad043
lakshya1333 <lakshyajain906@gmail.com>
2025-01-08 22:55:51 +0530

    Moved frontend code to frontend directory

---
+++ frontend/src/App.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [error, setError] = useState("");
+  const wf = useRef();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const roomId = queryParams.get("roomId");
+
+  useEffect(() => {
+    if (!roomId) {
+      alert("Room ID is required!");
+      return;
+    }
+
+    setMessages([`Hello, your room ID is: ${roomId}. You can share it with others to join this anonymous chatroom`]);
+
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+    
+        if (data.type === "error") {
+          setError(data.message);
+          return;
+        }
+    
+        if (data.type === "joined") {
+          setError("");
+        }
+    
+        if (data.type === "chat") {
+          setMessages((m) => [...m, data.payload.message]);
+        }
+      } catch (error) {
+        console.error("Failed to parse WebSocket message:", event.data, error);
+      }
+    };
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: roomId,
+          },
+        })
+      );
+    };
+
+    wf.current = ws;
+
+    return () => {
+      ws.close();
+    };
+  }, [roomId]);
+
+  return (
+    <div className="h-screen bg-black">
+      <br />
+      {error ? (
+        <div className="bg-red-500 text-white p-4 text-center">{error}</div>
+      ) : (
+
+        <>
+          <div className="h-[90vh] overflow-y-auto flex flex-col items-start">
+            {messages.map((message, index) => (
+              <div
+              key={index}
+              className="m-2 w-full flex"
+              >
+          <span
+            className="bg-white text-black rounded p-4 break-words max-w-[90%] w-fit"
+            >
+            {message}
+          </span>
+         </div>
+  ))}
+</div>
+
+          <div className="w-full bg-white flex">
+            <input id="message" className="flex-1 p-4" type="text"></input>
+            <button
+              onClick={() => {
+                const message = document.getElementById("message")?.value;
+                if (wf.current?.readyState === WebSocket.OPEN) {
+                  wf.current.send(
+                    JSON.stringify({
+                      type: "chat",
+                      payload: {
+                        message: message,
+                      },
+                    })
+                  );
+                }
+              }}
+              className="bg-purple-600 text-white p-4"
+            >
+              Send Message
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
+export function Login() {
+  const navigate = useNavigate();
+  const ws = useRef();
+
+  if (!ws.current) {
+    ws.current = new WebSocket("ws://localhost:8080");
+  }
+
+  function newroom() {
+    ws.current.send(
+      JSON.stringify({
+        type: "createRoom",
+      })
+    );
+
+    ws.current.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "roomCreated") {
+        const roomId = data.payload.roomId;
+        navigate(`/chat?roomId=${roomId}`)
+      }
+    };
+  }
+
+  return (
+    <div className="h-screen flex justify-center items-center bg-black">
+      <div className="h-60 w-96 bg-white rounded-3xl">
+        <div className="text-4xl font-extrabold p-6">Welcome to Chatty</div>
+        <div className="flex flex-row m-4">
+          <button
+            onClick={() => {
+              navigate("/join");
+            }}
+            className="mx-14 bg-lime-400 rounded-3xl p-3"
+          >
+            Join a room
+          </button>
+          <button onClick={newroom} className="bg-lime-400 rounded-3xl p-3">
+            Create a room
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
